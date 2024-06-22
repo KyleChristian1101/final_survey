4c65ed99f6b8ad7a95131a8182387db7d56be4a7
aRuUuX <venturarainier30@icloud.com>
2024-06-22 11:15:22 +0800

    revised thankyou

--- app/thankyou/page.tsx
+++ app/thankyou/page.tsx
@@ -1,21 +1,16 @@
-// ThankYouPage.tsx
-
 "use client"
 import React from 'react';
-import { useRouter } from 'next/router';
 import styles from './ThankYouPage.module.css'; // Import styles
-import Link from 'next/link';
 
 const ThankYouPage = () => {
   return (
     <div className={styles.thankYouContainer}>
       <div className={styles.card}>
+        <div className={styles.redLine}></div> {/* Red line on top */}
         <h2>Thank you for taking the survey!</h2>
         <hr className={styles.titleLine} />
         <p>Your responses have been recorded.</p>
-        <Link href="/survey">
-          <button className={styles.buttonSubmit}>Take Another Survey</button>
-        </Link>
+        <p>Your answers will remain private and confidential.</p>
       </div>
     </div>
   );
